5a7e8b6d765d95ceb85d3f60de4fe6722d7127c6
Gayatri jha <gayatrixreal@gmail.com>
2026-01-31 14:33:47 +0530

    4

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <h2>About Me</h2>
       <p>
         Hi, I am Gochutti. I build web applications using React and Node.js.
+        <img src="gochutti logo.png" alt="A descriptive piece of text about the image" width="500"></img>
       </p>
 
       <h2>Projects</h2>
@@ -21,6 +22,7 @@ function App() {
       <h2>Contact</h2>
       <p>Email: gochutti@gmail.com</p>
       <p>Phone no: 9650386565</p>
+
     </div>
   );
 }
